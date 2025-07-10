945989e0ed184e4e26a6e41535ba09b770bc9457
minwoo129 <key0129mw@naver.com>
2025-07-10 12:36:48 +0900

    feat: #13 mask > maskPhoneNumber 테스트 스크립트 작성

---
+++ __test__/mask.test.ts
@@ -0,0 +1,34 @@
+import { describe, expect, it } from 'vitest';
+import Mask from '../src/utils/mask';
+
+describe('mask > maskPhoneNumber', () => {
+  describe('하이픈이 없는 전화번호들에 대해 마스킹이 처리되는 경우', () => {
+    [
+      { input: '021234567', output: '02***4567' },
+      { input: '0212345678', output: '02****5678' },
+      { input: '0311234567', output: '031***4567' },
+      { input: '03112345678', output: '031****5678' },
+      { input: '0111234567', output: '011***4567' },
+      { input: '01112345678', output: '011****5678' },
+      { input: '01012345678', output: '010****5678' },
+    ].forEach(({ input, output }) => {
+      it(`input: ${input}, output: ${output}`, () => {
+        expect(Mask.maskPhoneNumber(input)).toBe(output);
+      });
+    });
+  });
+  describe('하이픈이 있는 전화번호들에 대해 마스킹이 처리되는 경우', () => {
+    [
+      { input: '02-123-4567', output: '02-***-4567' },
+      { input: '02-1234-5678', output: '02-****-5678' },
+      { input: '031-123-4567', output: '031-***-4567' },
+      { input: '031-1234-5678', output: '031-****-5678' },
+      { input: '011-123-4567', output: '011-***-4567' },
+      { input: '010-1234-5678', output: '010-****-5678' },
+    ].forEach(({ input, output }) => {
+      it(`input: ${input}, output: ${output}`, () => {
+        expect(Mask.maskPhoneNumber(input)).toBe(output);
+      });
+    });
+  });
+});
