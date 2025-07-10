1c783818a1a8f68357c0518238367e36c1f07531
minwoo129 <key0129mw@naver.com>
2025-07-10 12:25:21 +0900

    feat: #13 number > percent 테스트 스크립트 작성
    - 기능 수정 및 문서 내용 수정

--- __test__/number.test.ts
+++ __test__/number.test.ts
@@ -70,3 +70,43 @@ describe('number > sum', () => {
     expect(Number.sum(nums)).toBe(15);
   });
 });
+
+describe('number > percent', () => {
+  it('분자가 0인 경우', () => {
+    const num = 0;
+    const denom = 100;
+    expect(Number.percent(num, denom)).toBe('0');
+  });
+  it('분모가 0인 경우', () => {
+    const num = 100;
+    const denom = 0;
+    expect(Number.percent(num, denom)).toBe('error');
+  });
+  it('분자가 0보다 작은 경우', () => {
+    const num = -10;
+    const denom = 100;
+    expect(Number.percent(num, denom)).toBe('error');
+  });
+  it('분모가 0보다 작은 경우', () => {
+    const num = 5;
+    const denom = -100;
+    expect(Number.percent(num, denom)).toBe('error');
+  });
+  it('분자가 분모보다 큰 경우', () => {
+    const num = 100;
+    const denom = 50;
+    expect(Number.percent(num, denom)).toBe('error');
+  });
+  it('소숫점 아래 2자리까지 표현이 되는지 여부', () => {
+    const num = 100;
+    const denom = 200;
+
+    expect(Number.percent(num, denom, 2)).toBe('50.00');
+  });
+  it('소숫점 아래 3자리까지 표현이 되는지 여부', () => {
+    const num = 100;
+    const denom = 300;
+
+    expect(Number.percent(num, denom, 3)).toBe('33.333');
+  });
+});
