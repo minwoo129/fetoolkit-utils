1af090a1dd5acb78c2c5ce1a2e722fef93290527
minwoo129 <key0129mw@naver.com>
2025-07-15 12:47:14 +0900

    feat: #17 npm 배포를 위한 최종 설정 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,26 @@
+/// <reference types="vite/client" />
+import path from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+      entryRoot: 'src',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+      },
+      name: '@fetoolkit/utils',
+      fileName: 'index',
+      formats: ['es', 'umd'],
+    },
+  },
+  esbuild: {
+    jsx: 'automatic',
+  },
+});
