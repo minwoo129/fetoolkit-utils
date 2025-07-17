4bc6a9133cb923e11680de7d0b6c89d3bd2dfd66
minwoo129 <key0129mw@naver.com>
2025-07-17 13:16:28 +0900

    feat: #27 array 계열 유틸함수 esm 방식으로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as ArrayUtils } from './utils/array';
+export * from './utils/array';
 export { default as MaskUtils } from './utils/mask';
 export { default as NumberUtils } from './utils/number';
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 type FilterActionType<T> = (item: T, index: number) => boolean;
 // =============================================================
-const at = <T>(array: T[], at: number) => {
+export const at = <T>(array: T[], at: number) => {
   if (array.length === 0) {
     return undefined;
   }
@@ -13,25 +13,21 @@ const at = <T>(array: T[], at: number) => {
   return decIdx === 0 ? array[0] : array[array.length - decIdx];
 };
 
-const first = <T>(array: T[]): T | undefined => {
+export const first = <T>(array: T[]): T | undefined => {
   return array[0];
 };
 
-const last = <T>(array: T[]): T | undefined => {
+export const last = <T>(array: T[]): T | undefined => {
   return array[array.length - 1];
 };
 
-const filter = <T>(array: T[], filterBy: number[] | FilterActionType<T>) => {
+export const filter = <T>(
+  array: T[],
+  filterBy: number[] | FilterActionType<T>,
+) => {
   if (Array.isArray(filterBy)) {
     return array.filter((_, index) => filterBy.includes(index));
   }
 
   return array.filter(filterBy);
-};
-
-export default {
-  at,
-  first,
-  last,
-  filter,
 };
