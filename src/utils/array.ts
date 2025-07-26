1ea2790b081a011f4ea771c8c2084e93dcc72404
minwoo129 <key0129mw@naver.com>
2025-07-26 15:19:43 +0900

    feat: #34 at 함수 오류 수정
    - 정수가 들어오는 경우에만 기능을 처리하고, 그게 아니면 undefined로 반환처리

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -7,6 +7,10 @@ export const at = <T>(array: readonly T[], at: number) => {
   if (array.length === 0) {
     return undefined;
   }
+  if (!Number.isInteger(at)) {
+    return undefined;
+  }
+
   if (at > 0) {
     return array[at % array.length];
   }
