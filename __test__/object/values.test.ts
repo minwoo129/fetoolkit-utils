8e89c588e97739354d7b79f008f19ece0b84ee12
minwoo129 <key0129mw@naver.com>
2025-07-27 15:47:43 +0900

    feat: #39 object > values 테스트 케이스 추가

--- __test__/object/values.test.ts
+++ __test__/object/values.test.ts
@@ -13,4 +13,31 @@ describe('object > values', () => {
 
     expect(result).toEqual([1, 2, 3]);
   });
+
+  it("'length' 라는 key값을 가지고 있는 객체에서 동작하는지 여부", () => {
+    const testObj = {
+      0: 'a',
+      1: 'b',
+      length: 2,
+    };
+
+    const result = values(testObj).sort();
+
+    expect(result).toEqual([2, 'a', 'b']);
+  });
+
+  it('상속된 문자열 키 속성값을 미포함하는지 여부', () => {
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-explicit-any
+    function Foo(this: any) {
+      this.a = 1;
+    }
+    Foo.prototype.b = 2;
+
+    const expected = [1];
+    // eslint-disable-next-line
+    // @ts-ignore
+    const actual = values(new Foo()).sort();
+
+    expect(actual).toEqual(expected);
+  });
 });
